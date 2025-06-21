95811c99fbf33d72d25fe2501ada8b9f30b66722
rubenaguilardev <ragui56@wgu.edu>
2025-06-21 02:05:56 -0700

    updates

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -4,12 +4,12 @@ const About = () => {
     return (
         <section id='about' className="flex items-center justify-center mb-40">
             <div className="max-w-2xl md:max-w-[44rem] lg:max-w-[58rem] mx-auto px-4">
-                <h2 className="text-3xl text-center font-bold mb-8 text-gray-300">About <span className="bg-gradient-to-r from-blue-500 to-purple-600 
+                <h2 className="text-3xl text-center font-bold mb-7 text-gray-300">About <span className="bg-gradient-to-r from-blue-500 to-purple-600 
                 bg-clip-text text-transparent">Me</span></h2>
                 
                 <div className="flex flex-col md:flex-row glass rounded-xl p-7 border-white/10 border hover:-translate-y-1 transition-all">
                     
-                    <div className="flex w-full mb-6 sm:pr-6 md:mb-0">
+                    <div className="flex w-full mb-7 sm:pr-7 md:mb-0">
                         <img src={profilePicture} alt="picture of Jazmin" className="rounded-xl shadow-xl"/>
                     </div>
                     <div className="flex flex-col justify-center md:w-3/4 text-gray-300">

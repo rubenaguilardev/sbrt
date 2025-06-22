7ef8b4dfd7c6c216128a70084332a948af6ac706
rubenaguilardev <ragui56@wgu.edu>
2025-06-22 15:12:39 -0700

    update work sections

--- src/components/sections/WorkSections.jsx
+++ src/components/sections/WorkSections.jsx
@@ -13,24 +13,24 @@ const WorkSections = () => {
             id={index} 
             label={section.label}
             instagram={section.id === 0 && section.content.map((item, index) => (
-                <div key={index} className="snap-center shrink-0 max-w-md rounded-lg">
+                <div key={index} className="snap-center shrink-0 max-w-md rounded-lg transition-transform duration-300 hover:scale-102 mt-2">
                     <InstagramEmbed url={item}/> 
                 </div>
             ))}
 
             design={section.id === 1 && section.content.map((item, index) => (
-                <div key={index} className="cursor-pointer w-[400px] h-[400px] overflow-hidden rounded-lg flex-shrink-0" onClick={() => setSelectedImage(item)}>
+                <div key={index} className="cursor-pointer w-[300px] h-[350px] overflow-hidden rounded-lg flex-shrink-0" onClick={() => setSelectedImage(item)}>
                     <img key={index} src={item} alt={`design picture ${index}`} className="w-[140%] h-full object-cover object-top transition-transform duration-300 hover:scale-105"/>
                 </div>
             ))}
 
             blogs={section.id === 2 && section.content.map((post, index) => (
                     <a href={post.url}>
-                        <div key={index} className='flex flex-col pb-3'>
-                            <div className='w-[240px] md:w-[280px] object-fit'>
+                        <div key={index} className='flex flex-col pb-3 transition-transform duration-300 hover:scale-102 mt-1'>
+                            <div className='w-[220px] md:w-[260px] object-fit'>
                                 <img src={post.image} className='rounded-t-lg'/>
                             </div>
-                            <p className='bg-beige rounded-b-lg text-gray-700 text-center p-6 xl:p-8 text-sm bg-[whitesmoke]'>{post.description}</p>
+                            <p className='max-w-[260px] rounded-b-lg text-gray-700 text-center p-3 xl:p-5 text-sm bg-[whitesmoke]'>{post.description}</p>
                         </div>
                     </a>
             ))}
@@ -48,7 +48,7 @@ const WorkSections = () => {
                     <img src={selectedImage} alt='full view' className='max-w-full max-h-full rounded-lg' 
                     onClick={(e) => e.stopPropagation()}
                     />
-                    <button onClick={() => setSelectedImage(null)} className='absolute top-6 right-6 text-white text-2xl'>&times;</button>
+                    <button onClick={() => setSelectedImage(null)} className='absolute top-6 right-6 text-blue-500 text-2xl'>&times;</button>
                 </div>
                 )}
              </div>

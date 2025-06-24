2d2f029917e7b93a56019c1b1f9ff7884f62f413
rubenaguilardev <ragui56@wgu.edu>
2025-06-24 02:54:02 -0700

    add footer icons

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -6,17 +6,19 @@ const Footer = () => {
         <section id='about' className="mb-5">
             <div className="max-w-2xl md:max-w-[44rem] lg:max-w-[58rem] xl:max-w-6xl mx-auto px-4">
                 <div className='flex justify-between'>
-                <div className='md:hidden text-blue-500 text-4xl'>
-                     <FaLinkedin/>
-                </div>
-               
-                <p className='text-gray-300 text-xs self-end'>{new Date().getFullYear()} Social Buffington. All rights reserved.</p>
-                <div className='md:hidden text-blue-500 text-4xl'>
-                     <FaEnvelope/>
-                </div>
+                    <div className='md:hidden text-blue-500 text-4xl'>
+                        <a href="https://www.linkedin.com/in/jazmin-aguilar-buffington/" className="mb-4 hover:text-gray-300 text-4xl hover:-translate-y-1 transition-all">
+                            <FaLinkedin />
+                        </a>
+                    </div>
+                    <div className='md:hidden text-blue-500 text-4xl'>
+                        <a href="mailto:jazmin.a.buffington@gmail.com" className="mb-4 hover:text-gray-300 text-4xl hover:-translate-y-1 transition-all">
+                            <FaEnvelope/>
+                        </a>
+                    </div>
                 </div>
             </div>
-            
+            <p className='text-gray-300 text-xs text-center mt-2'>{new Date().getFullYear()} Social Buffington. All rights reserved.</p>
         </section>
     )
 }

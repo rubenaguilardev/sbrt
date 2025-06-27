ae3226652a05bc4f69dacb99161af33d4a1beb62
rubenaguilardev <ragui56@wgu.edu>
2025-06-27 14:27:12 -0700

    update gallery

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -7,7 +7,7 @@ import "yet-another-react-lightbox/styles.css";
 import RevealOnScroll from "../RevealOnScroll"
 
 const responsive = {
-  desktop: { breakpoint: { max: 3000, min: 1024 }, items: 1 },
+  desktop: { breakpoint: { max: 3000, min: 1024 }, items: 2 },
   tablet:  { breakpoint: { max: 1024, min: 464 },  items: 1 },
   mobile:  { breakpoint: { max: 464,  min: 0 },    items: 1 },
 };

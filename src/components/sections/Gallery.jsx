610bc5f5a457e1d02faa8a819fe3b413730fd00c
rubenaguilardev <ragui56@wgu.edu>
2025-08-19 01:18:47 -0700

    final update for now

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -4,6 +4,7 @@ import AutoScroll from 'embla-carousel-auto-scroll'
 import {PHOTOS} from '../../constants/gallery'
 import Lightbox from "yet-another-react-lightbox"
 import "yet-another-react-lightbox/styles.css"
+import RevealOnScroll from "../RevealOnScroll"
 
 
 
@@ -104,62 +105,64 @@ return (
   }, [emblaApi, tweenOpacity])
 
   return (
-     <section id="gallery" className="flex flex-col justify-center items-center mb-30">
-      <h2 className="text-3xl text-center font-bold mb-5 md:mb-7 text-gray-300">My <span className="bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">Photography </span></h2>
-      <div className="embla relative w-full md:max-w-[44rem] lg:max-w-[58rem] max-w-6xl xl:max-w-[72.5rem] md:px-1 mb-3">
-        <div className="embla__viewport" ref={emblaRef}>
-          <div className="embla__container">
-            {PHOTOS.map((item, index) => (
-              <div className="embla__slide" key={index}>
-                <img
-                  loading="lazy"
-                  className="embla__slide__img cursor-zoom-in rounded-lg"
-                  src={item.sm}
-                  alt={`photo ${index}`}
-                  onClick={() => {
-                    setPhotoIndex(index)
-                    setOpen(true)
-                  }}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      <ArrowButton direction="left" onClick={() => emblaApi?.scrollPrev()} />
-      <ArrowButton direction="right" onClick={() => emblaApi?.scrollNext()} />
-      </div>
-      
-      {open && (
-            <Lightbox
-              open={open}
-              close={() => setOpen(false)}
-              index={photoIndex}
-              slides={PHOTOS.map((p) => ({ src: p.img }))}
-            />
-          )}
-
-          <div className="w-full md:max-w-[44rem] lg:max-w-[58rem] max-w-6xl xl:max-w-[72.5rem] md:px-1 mt-[.6rem] md:mt-[.7rem] lg:mt-[.8rem]">
-          <div className="embla__viewport">
-          <div className="embla-thumb pointer-events-none" ref={emblaThumbsRef}>
+    <RevealOnScroll>
+      <section id="gallery" className="flex flex-col justify-center items-center mb-30">
+        <h2 className="text-3xl text-center font-bold mb-5 md:mb-7 text-gray-300">My <span className="bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">Photography </span></h2>
+        <div className="embla relative w-full md:max-w-[44rem] lg:max-w-[58rem] max-w-6xl xl:max-w-[72.5rem] md:px-1 mb-3">
+          <div className="embla__viewport" ref={emblaRef}>
             <div className="embla__container">
               {PHOTOS.map((item, index) => (
-                <div
-                  key={item.id}
-                  className="embla__slide"
-                >
+                <div className="embla__slide" key={index}>
                   <img
                     loading="lazy"
-                    src={item.xs}
-                    alt={`${index}`}
-                    className="w-full h-40 object-cover md:rounded-lg rounded-lg transition-all duration-300 ease-in hover:scale-105"
+                    className="embla__slide__img cursor-zoom-in rounded-lg"
+                    src={item.sm}
+                    alt={`photo ${index}`}
+                    onClick={() => {
+                      setPhotoIndex(index)
+                      setOpen(true)
+                    }}
                   />
                 </div>
               ))}
             </div>
           </div>
-          </div>
-          </div>
-    </section>
+        <ArrowButton direction="left" onClick={() => emblaApi?.scrollPrev()} />
+        <ArrowButton direction="right" onClick={() => emblaApi?.scrollNext()} />
+        </div>
+        
+        {open && (
+              <Lightbox
+                open={open}
+                close={() => setOpen(false)}
+                index={photoIndex}
+                slides={PHOTOS.map((p) => ({ src: p.img }))}
+              />
+            )}
+
+            <div className="w-full md:max-w-[44rem] lg:max-w-[58rem] max-w-6xl xl:max-w-[72.5rem] md:px-1 mt-[.6rem] md:mt-[.7rem] lg:mt-[.8rem]">
+            <div className="embla__viewport">
+            <div className="embla-thumb pointer-events-none" ref={emblaThumbsRef}>
+              <div className="embla__container">
+                {PHOTOS.map((item, index) => (
+                  <div
+                    key={item.id}
+                    className="embla__slide"
+                  >
+                    <img
+                      loading="lazy"
+                      src={item.xs}
+                      alt={`${index}`}
+                      className="w-full h-40 object-cover md:rounded-lg rounded-lg transition-all duration-300 ease-in hover:scale-105"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+            </div>
+            </div>
+      </section>
+    </RevealOnScroll>
     
   )
 }
